73f04a19b34cba94ab2cfd801c3377fc146049a7
joe mckinney <mckinney.a.joe@gmail.com>
2018-05-04 13:12:59 -0700

    update project for seperation of concerns.  I created a component called article-list whos sole responsibility is to display the articles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ArticleListComponent } from './article-list/article-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/article-list/article-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Article } from '../models/article.model';
+@Component({
+  selector: 'app-article-list',
+  templateUrl: './article-list.component.html',
+  styleUrls: ['./article-list.component.css']
+})
+export class ArticleListComponent {
+  @Input() childArticleList: Article[];
+  @Output() clickSender = new EventEmitter();
+
+  viewButtonClicked(articleToView: Article) {
+    this.clickSender.emit(articleToView);
+  }
+
+  viewsColor(currentArticle){
+    if (currentArticle.views > 15) {
+      return "lots-of-views";
+    } else {
+      return "normal-views";
+    }
+  }
+  constructor() { }
+
+}
