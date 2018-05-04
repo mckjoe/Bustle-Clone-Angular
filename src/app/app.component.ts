5227ad758a19dcdbe6706e3a817e9715155699fc
joe mckinney <mckinney.a.joe@gmail.com>
2018-05-04 10:25:46 -0700

    update root component to make sure constructor works as expected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { Article } from './models/article.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  firstArticle: Article = new Article("Example Title", "Welcome to bustleClone the website!", "Joseph McKinney", "This webpage is a webpage that allows users to view articles.  This is a pretend article that you are reading right now it has no relevence.")
 }
