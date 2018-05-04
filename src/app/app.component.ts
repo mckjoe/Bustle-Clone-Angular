3c98fd0d3002b75a3573ac9af2499f4cea921f35
joe mckinney <mckinney.a.joe@gmail.com>
2018-05-04 10:36:44 -0700

    update root component with hard coded example articles that I can later edit into more article like displays, but for now the page will display to user the articles that were hard coded in as a title, a brief description, and an author

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Article } from './models/article.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  firstArticle: Article = new Article("Example Title", "Welcome to bustleClone the website!", "Joseph McKinney", "This webpage is a webpage that allows users to view articles.  This is a pretend article that you are reading right now it has no relevence.")
+  articles: Article[] = [
+    new Article("Example Title", "Welcome to bustleClone the website!", "Joseph McKinney", "This webpage is a webpage that allows users to view articles.  This is a pretend article that you are reading right now it has no relevence."),
+    new Article("Example 2", "this is another example", "Random Author", "This is another pretend article, hopefully it has been for of education"),
+    new Article("Example 3", "This is a third example", "Joseph Author", "this is the body of this new third article that people can read")
+  ];
 }
